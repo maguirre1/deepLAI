e9a1084993b07548f1b98bede9ff78181c5de1f0
Matthew Aguirre <magu@sh01-ln04.stanford.edu>
2020-07-05 15:18:19 -0700

    fixed typo

--- model/train.sh
+++ model/train.sh
@@ -21,7 +21,7 @@ else
 	ml purge; 
 	ml load python/3.6.1 cuda/10.2.89 cudnn/7.6.4 py-scipy/1.4.1_py36;
 	ml load py-scipy/1.4.1_py36 py-numpy/1.18.1_py36;
-	ml load py-tensorflow/2.1.0_py3 py-pandas/1.0.3_py36;
+	ml load py-tensorflow/2.1.0_py36 py-pandas/1.0.3_py36;
 	nvidia-smi --query-gpu=index,name --format=csv,noheader
 	which python3
 	ml list
