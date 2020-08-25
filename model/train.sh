851f2f5f9da1cb0a9aac329c0c60336372dd2237
Matthew Aguirre <magu@sh03-13n02.int>
2020-08-24 18:57:37 -0700

    misc updates

--- model/train.sh
+++ model/train.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH -J segnet
 #SBATCH -p gpu,owners
-#SBATCH --gpus=4 
-#SBATCH -t 24:00:00
+#SBATCH --gpus=1 
+#SBATCH -C GPU_MEM:32GB
+#SBATCH -t 48:00:00
 #SBATCH --cpus-per-gpu=1
-#SBATCH --mem-per-gpu=16G
-
+#SBATCH --mem-per-gpu=32G
+#SBATCH --out logs/train_%A.out
 
 __README="""
 A wrapper script for train.py
@@ -15,27 +16,26 @@ A wrapper script for train.py
 # first check if we're on galangal
 hn=$(hostname)
 if [ $hn = "galangal.stanford.edu" ]; then 
-	nvidia-smi
+  nvidia-smi
 else
-	# if not then fiddle around with modules
-	ml purge; 
-	ml load python/3.6.1 cuda/10.1.168 cudnn/7.4.1.5; # openmpi/4.0.3
-	ml load py-numpy/1.18.1_py36 py-scipy/1.4.1_py36 py-tensorflow/2.0.0_py36;
-	# display node info
-	nvidia-smi --query-gpu=index,name --format=csv,noheader
-	nvidia-smi
-	# display packages
-	which python3
-	ml list
+  # if not then fiddle around with modules
+  ml purge; 
+  ml load python/3.6.1 cuda/10.1.168 cudnn/7.4.1.5; # openmpi/4.0.3
+  ml load py-numpy/1.18.1_py36 py-scipy/1.4.1_py36 py-tensorflow/2.0.0_py36;
+  # display node info
+  nvidia-smi --query-gpu=index,name --format=csv,noheader
+  nvidia-smi
+  # display packages
+  which python3
+  ml list
 fi
 
 
 # call the model training script
-python3 train.py --out test_segnet_chr20 --no-generator --dropout-rate=0.0 --input-dropout-rate=0.05 
-
-## --batch-size=16 --num-filters=8 --filter-size=8 --pool-size=6 --input-dropout-rate=0.01 --dropout-rate=0.0 --num-blocks=5
-
-
+python3 train.py --out chr20p_short --array-only \
+  --filter-size=8 --num-filter=12 --num-blocks=4 --batch-size=128
+ # --bp-start=1 --bp-end=28100000 \
+ 
 PARAMS="""
   --chrom 20            Chromosome to use (must be in 1,2,...,22)
   --batch-size 4        Minibatch size for training
