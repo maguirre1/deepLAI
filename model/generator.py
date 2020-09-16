2f1ca4fced0e8ea09f6a48bcd79edb7a3dced5f1
Matthew Aguirre <magu@sh02-16n06.int>
2020-09-16 13:52:02 -0700

    added frequency-based sampling to generator

--- model/generator.py
+++ model/generator.py
@@ -1,5 +1,6 @@
 import numpy as np
-import tensorflow.keras as keras
+import tensorflow as tf
+from tensorflow import keras
 import scipy.stats as ss
 from tensorflow.keras.utils import to_categorical
 
@@ -25,17 +26,17 @@ class DataLoader(keras.utils.Sequence):
 		ss=self.ids[int(ix*self.n):int((ix+1)*self.n)]
 		Xs=np.stack([np.load(self.path+'/'+s+'.G.npy')[self.var,:self.l] for s in ss])
 		Ys=np.stack([np.load(self.path+'/'+s+'.L.npy')[self.var,:self.m] for s in ss])
-		return (Xs,Ys) if not self.admix else naive_admixing(Xs,Ys)
+		return (Xs, Ys) if not self.admix else naive_admixing(Xs,Ys)
 	def on_epoch_end(self):
 		# shuffle ids at the end of an epoch (optionally)
 		if self.shuffle:
-			np.random.shuffle(self.ids)	
-	
+			np.random.shuffle(self.ids)
+
 
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
     def __init__(self, train_ix, batch_size, dim, n_alleles, n_classes, X, Y, 
-                 shuffle=True, admix=False, v1=0, v2=None):
+                 shuffle=True, sample=False, admix=False, v1=0, v2=None, anc_wts=None):
         'Initialization'
         self.dim = dim
         self.batch_size = batch_size
@@ -51,6 +52,10 @@ class DataGenerator(keras.utils.Sequence):
         self.indexes = np.arange(self.list_IDs.shape[0])
         self.v1 = v1
         self.v2 = dim if v2 is None else v2
+        self.sample = sample
+        self.a_prob = np.arange(n_classes)/float(n_classes) if anc_wts is None else anc_wts
+        self.y_prob = np.array([self.a_prob[np.where(Y[i,0,:n_classes])[0][0]] for i in range(Y.shape[0])])
+        self.y_prob /= np.sum(self.y_prob)
         self.on_epoch_end()
 
     def __len__(self):
@@ -60,32 +65,34 @@ class DataGenerator(keras.utils.Sequence):
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
-        indexes = np.arange(index*self.batch_size, (index+1)*self.batch_size)
+        if self.sample:
+            indexes = np.random.choice(self.indexes, size=self.batch_size, replace=True, p=self.y_prob)
+        else:
+            indexes = np.arange(index*self.batch_size, (index+1)*self.batch_size)
         # Find list of IDs
-        list_IDs_temp = np.array([self.list_IDs[k] for k in indexes])
-        return self.__data_generation(list_IDs_temp)
+        ids = np.array([self.list_IDs[k] for k in indexes])
+        return self.__data_generation(ids)
 
     def on_epoch_end(self):
         'Updates indexes after each epoch'
-        self.indexes = np.arange(self.list_IDs.shape[0])
-        if self.shuffle:
+        # self.indexes = np.arange(self.list_IDs.shape[0])
+        if self.shuffle and not self.sample:
             np.random.shuffle(self.indexes)
-
-    def __data_generation(self, list_IDs_temp):
+    
+    def __data_generation(self, ids):
         'Generates data containing batch_size samples' 
         # X : (n_samples, *dim, n_channels)
-        X_sm = self.X[list_IDs_temp, self.v1:self.v2, :self.n_alleles] 
-        y_sm = self.Y[list_IDs_temp, self.v1:self.v2, :]         
+        X_sm = self.X[ids, self.v1:self.v2, :self.n_alleles] 
+        y_sm = self.Y[ids, self.v1:self.v2, :]         
         if self.admix:
             return naive_admixing(X_sm, y_sm)
         else:
             return X_sm, y_sm
 
 
-def naive_admixing(X_data, Y_data):
+def naive_admixing(X_data, Y_data, maxgen=3):
     'Takes X_data and Y_data (labels) and returns naively admixed samples'
     n_fake=X_data.shape[0]
-    maxgen=3
     n_splits=2+np.hstack([ss.poisson.rvs(2.86*gen, size=n_fake//maxgen) for gen in range(1,maxgen)])
 
     # new individuals
